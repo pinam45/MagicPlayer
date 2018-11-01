3279378cfa19e5cf47b78891166e92f272e0b09a
Maxime Pinard <maxime.pin@live.fr>
2018-11-01 16:47:24 -0400

    Added CMake config for utfcpp

---
+++ cmake/utfcpp.cmake
@@ -0,0 +1,17 @@
+message(STATUS "Configuring utfcpp")
+
+get_filename_component(UTFCPP_DIR ${CMAKE_SOURCE_DIR}/deps/utfcpp ABSOLUTE)
+
+# Submodule check
+directory_is_empty(is_empty "${UTFCPP_DIR}")
+if(is_empty)
+	message(FATAL_ERROR "utfcpp dependency is missing, maybe you didn't pull the git submodules")
+endif()
+
+# Variables
+get_filename_component(UTFCPP_INCLUDE_DIR  ${UTFCPP_DIR}/source  ABSOLUTE)
+set(UTFCPP_LIBRARY "")
+
+# Message
+message("> include: ${UTFCPP_INCLUDE_DIR}")
+message(STATUS "Configuring utfcpp - Done")
